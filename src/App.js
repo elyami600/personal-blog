bf1119b117adca228744c0ac2672e4bf18b55f7c
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2025-03-15 15:00:37 -0400

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Blog from './BlogPage';
 import NavigationBar from './Nav';
 import About from './AboutPage';
 
 function App() {
   return (
-    <div className="App">
-      <NavigationBar />
-      <Routes>
-        <Route path="/" element={<Blog />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </div>
+    <Router>
+      <div className="App">
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Blog />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
